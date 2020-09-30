35f535907d4adcc67ef91a518cbce43e993240e4
Romesh Singhabahu <romesh@rosin355.com>
2020-09-30 11:33:36 +0200

    Initial Commit

---
+++ Stylepoll/StylepollApp.swift
@@ -0,0 +1,17 @@
+//
+//  StylepollApp.swift
+//  Stylepoll
+//
+//  Created by Romesh Singhabahu on 30/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct StylepollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
